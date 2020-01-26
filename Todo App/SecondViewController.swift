7dab592ab751556b8978b95f5fb8cc3e44d8bbb4
elnatan <elnatan@ravtech.co.il>
2020-01-26 15:19:23 +0200

    Initial Commit

---
+++ Todo App/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Todo App
+//
+//  Created by admin on 26/01/2020.
+//  Copyright © 2020 AM Kirsch. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
